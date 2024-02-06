12f50ac920fbedd542c29591e4c4e1d9cb19de2a
EC2 Default User <ec2-user@ip-172-31-2-144.ap-northeast-1.compute.internal>
2024-02-06 10:24:17 +0000

    [finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,11 +3,11 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを作成する
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -15,11 +15,12 @@ class ListsController < ApplicationController
   end
 
   def syow
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title,:body)
